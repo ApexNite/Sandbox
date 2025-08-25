f53f500ff73f907a78809c903d57774efba67004
ApexNite <128202315+ApexNite@users.noreply.github.com>
2025-08-25 05:41:44 -0700

    Various compatability changes

--- UI/BuildingConstructorSelector.cs
+++ UI/BuildingConstructorSelector.cs
@@ -1,26 +1,56 @@
-﻿using NeoModLoader.General;
+﻿using System.Collections.Generic;
+using NeoModLoader.General;
 using NeoModLoader.General.UI.Window;
 using NeoModLoader.General.UI.Window.Layout;
 using NeoModLoader.General.UI.Window.Utils.Extensions;
+using Sandbox.Features;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Sandbox.UI {
     internal class BuildingConstructorSelector : AutoLayoutWindow<BuildingConstructorSelector> {
-        protected override void Init() {
-            AutoGridLayoutGroup grid = this.BeginGridGroup(6, pCellSize: new Vector2(32, 32));
+        private AutoGridLayoutGroup _grid;
+        private List<string> _loadedBuildings;
 
+        public override void OnNormalEnable() {
+            BuildingConstructor.UpdateAssets();
+            UpdateButtons();
+        }
+
+        protected override void Init() {
+            _grid = this.BeginGridGroup(6, pCellSize: new Vector2(32, 32));
+            _loadedBuildings = new List<string>();
+
+            UpdateButtons();
+        }
+
+        private void UpdateButtons() {
             foreach (BuildingAsset buildingAsset in AssetManager.buildings.list) {
                 string id = $"place_{buildingAsset.id}";
 
+                if (_loadedBuildings.Contains(id)) {
+                    continue;
+                }
+
                 buildingAsset.checkSpritesAreLoaded();
 
-                Sprite sprite = buildingAsset.building_sprites.animation_data.GetRandom().main.GetRandom();
+                Sprite sprite = SpriteTextureLoader.getSprite("ui/icons/iconQuestionMark");
+                List<BuildingAnimationData> animations = buildingAsset.building_sprites.animation_data;
+
+                if (animations != null && animations.Count > 0) {
+                    BuildingAnimationData animationData = animations[0];
+
+                    if (animationData != null && animationData.main.Length > 0) {
+                        sprite = animationData.main.GetRandom();
+                    }
+                }
+
                 PowerButton powerButton = PowerButtonCreator.CreateGodPowerButton(id, sprite);
                 powerButton.gameObject.GetComponent<Button>()
                     .onClick.AddListener(() => ScrollWindow.getCurrentWindow().clickHide());
 
-                grid.AddChild(powerButton.gameObject);
+                _grid.AddChild(powerButton.gameObject);
+                _loadedBuildings.Add(id);
             }
         }
     }
--- UI/MagnetPlusEditor.cs
+++ UI/MagnetPlusEditor.cs
@@ -1,4 +1,5 @@
-﻿using NeoModLoader.General;
+﻿using System.Collections.Generic;
+using NeoModLoader.General;
 using NeoModLoader.General.UI.Window;
 using NeoModLoader.General.UI.Window.Layout;
 using NeoModLoader.General.UI.Window.Utils.Extensions;
@@ -6,15 +7,54 @@ using UnityEngine;
 
 namespace Sandbox.UI {
     internal class MagnetPlusEditor : AutoLayoutWindow<MagnetPlusEditor> {
-        protected override void Init() {
-            AutoGridLayoutGroup grid = this.BeginGridGroup(6, pCellSize: new Vector2(32, 32));
+        private AutoGridLayoutGroup _grid;
+        private List<string> _loadedActors;
 
+        public override void OnNormalEnable() {
+            UpdateButtons();
+        }
+
+        protected override void Init() {
+            _grid = this.BeginGridGroup(6, pCellSize: new Vector2(32, 32));
+            _loadedActors = new List<string>();
+
+            UpdateButtons();
+        }
+
+        private static Sprite GetAssetIdleSprite(ActorAsset actorAsset) {
+            string path = actorAsset.texture_asset.texture_path_base;
+            AnimationContainerUnit animation = new AnimationContainerUnit(path);
+
+            return actorAsset.animation_idle.Length == 0 ? null : animation.sprites[actorAsset.animation_idle[0]];
+        }
+
+        private static Sprite GetBoatSprite(ActorAsset actorAsset) {
+            return SpriteTextureLoader.getSpriteList($"actors/boats/{actorAsset.boat_texture_id}")[0];
+        }
+
+        private static bool HasNoUniqueIcon(ActorAsset actorAsset) {
+            return actorAsset.unit_zombie && actorAsset.id != "zombie"
+                   || actorAsset.kingdom_id_wild == "mush"
+                   || actorAsset.kingdom_id_wild == "tumor"
+                   || actorAsset.base_asset_id == "fire_elemental";
+        }
+
+        private void UpdateButtons() {
             foreach (ActorAsset actorAsset in AssetManager.actor_library.list) {
                 if (!actorAsset.can_be_moved_by_powers || actorAsset.id == "fire_elemental") {
                     continue;
                 }
 
                 string id = $"{actorAsset.id}_magnet_toggle";
+
+                if (_loadedActors.Contains(id)) {
+                    continue;
+                }
+
+                if (!LocalizedTextManager.instance.contains(id)) {
+                    LocalizedTextManager.add(id, id);
+                }
+
                 Sprite sprite;
 
                 if (actorAsset.id == "zombie_dragon") {
@@ -29,27 +69,14 @@ namespace Sandbox.UI {
                     sprite = actorAsset.getSpriteIcon();
                 }
 
+                if (sprite == null) {
+                    sprite = SpriteTextureLoader.getSprite("ui/icons/iconQuestionMark");
+                }
+
                 AssetManager.powers.add(new GodPower { id = id, name = id, toggle_name = id });
-                grid.AddChild(PowerButtonCreator.CreateToggleButton(id, sprite).gameObject);
+                _grid.AddChild(PowerButtonCreator.CreateToggleButton(id, sprite).gameObject);
+                _loadedActors.Add(id);
             }
-        }
-
-        private static Sprite GetAssetIdleSprite(ActorAsset actorAsset) {
-            string path = actorAsset.texture_asset.texture_path_base;
-            AnimationContainerUnit animation = new AnimationContainerUnit(path);
-
-            return animation.sprites[actorAsset.animation_idle[0]];
-        }
-
-        private static Sprite GetBoatSprite(ActorAsset actorAsset) {
-            return SpriteTextureLoader.getSpriteList($"actors/boats/{actorAsset.boat_texture_id}")[0];
-        }
-
-        private static bool HasNoUniqueIcon(ActorAsset actorAsset) {
-            return actorAsset.unit_zombie && actorAsset.id != "zombie"
-                   || actorAsset.kingdom_id_wild == "mush"
-                   || actorAsset.kingdom_id_wild == "tumor"
-                   || actorAsset.base_asset_id == "fire_elemental";
         }
     }
 }
